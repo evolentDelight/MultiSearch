86513c43dd557ddb05a9b92f0dc76d129a7e5e35
evolentDelight <evolentDelight@outlook.com>
2020-06-15 13:49:32 -0700

    Prototype Logic-100% CSS-10% Server-0% - Incomplete due to unable to access API

---
+++ Prototype/ms-prototype/src/components/Display.js
@@ -0,0 +1,19 @@
+import React from "react";
+import {nanoid} from "nanoid";
+import Store from "./Store.js"
+import "./Display.css";
+
+function Display(props){
+
+  const stores = props.storeList.map((store)=>
+    <Store key={`store-${nanoid()}`} name={store.name} link={store.link} product={props.searchValue} />
+  )
+
+  return(
+    <div className="display-flex">
+      {stores}
+    </div>
+  )
+}
+
+export default Display;
